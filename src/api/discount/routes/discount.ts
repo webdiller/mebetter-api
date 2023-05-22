c946809ffb0f1c943e9e0f72e6aa6a8e4b59a310
Eugene Butkov <eugenefromrus@gmail.com>
2023-05-22 22:15:49 +1000

    feat: init

---
+++ src/api/discount/routes/discount.ts
@@ -0,0 +1,7 @@
+/**
+ * discount router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::discount.discount');
---
+++ src/api/discount/services/discount.ts
@@ -0,0 +1,7 @@
+/**
+ * discount service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::discount.discount');
